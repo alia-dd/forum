9dbd52fe7dcae514dc9f917909febd81eefef7ce
Jyrki <279088161+jyrki-k@users.noreply.github.com>
2026-09-17 16:31:10 +0300

    add error template functionality, add delete post

--- handlers/auth_handler.go
+++ handlers/auth_handler.go
@@ -18,7 +18,7 @@ func (h *UseHandler) SignInUser(w http.ResponseWriter, r *http.Request) {
 	cx := r.Context()
 
 	if parseErr := r.ParseForm(); parseErr != nil {
-		handleError(w, customerrors.ErrInternalError)
+		handleError(w, r, customerrors.ErrInternalError)
 	}
 	userData := models.UserLogin{
 		Username: strings.TrimSpace(r.FormValue("username")),
@@ -27,7 +27,7 @@ func (h *UseHandler) SignInUser(w http.ResponseWriter, r *http.Request) {
 
 	session, siginErr := h.service.AuthenticateUserService(cx, userData)
 	if siginErr != nil {
-		handleError(w, siginErr)
+		handleError(w, r, siginErr)
 		return
 	}
 
@@ -51,7 +51,7 @@ func (h *UseHandler) SignOutUser(w http.ResponseWriter, r *http.Request) {
 	if cookieErr == nil {
 		if DeleteSessionErr := h.service.LogoutService(cx, cookie.Value); DeleteSessionErr != nil {
 			if !errors.Is(DeleteSessionErr, customerrors.ErrNotFound) {
-				handleError(w, DeleteSessionErr)
+				handleError(w, r, DeleteSessionErr)
 				return
 			}
 		}
